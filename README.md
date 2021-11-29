27a4b92ab6441d91d0fdbbb92deeb5a9c3fc67ed
christoph.berst <christoph.berst@stud.tu-darmstadt.de>
2021-11-29 19:48:43 +0100

    changed timezone to CET added preliminary models Course, Topic, Student made migrations for course app registered Course in admin.py created first admin user admin1

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Seminarplatzvergabe
 
-Bachelor Praktikum Thema 06  Seminarplatzvergabe
+Bachelor Praktikum Thema 06 Seminarplatzvergabe
 
-Emre Berber war hier
-Ich auch
+<h2>Description</h2>
+This is a test description. What could this readme file be used for?
+Maybe a description
+
+<h2>Current State</h2>
+<p>after steps of Tutorial 2</p>
+https://docs.djangoproject.com/en/3.2/intro/tutorial03/
+
+<h2>User Story Ideas</h2>
+<p>deal with timezones</p>
+https://docs.djangoproject.com/en/3.2/topics/i18n/timezones/
+
--- ppsv/course/models.py
+++ ppsv/course/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=200)
+    course_type = models.CharField(max_length=200)
+    register_from = models.DateTimeField('date registration opens')
+    registration_deadline = models.DateTimeField('registration deadline')
+    course_description = models.TextField('course description')
+    max_participants = models.IntegerField('maximum number of participants', default=9999)
+    cp = models.IntegerField('CP')
+    # maybe use choices for category and field?
+    category = models.CharField(max_length=200)
+    faculty = models.CharField(max_length=200)
+    # organizer or instructors?
+    organizer = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.course_name
+
+
+def course_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/course_<id>/<filename>
+    return 'course_{0}/{1}'.format(instance.course.id, filename)
+
+
+class Topic(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    topic_name = models.CharField(max_length=200)
+    max_participants = models.IntegerField('maximum number of participants', default=999)
+    topic_description = models.TextField('topic description')
+    topic_file = models.FileField(upload_to='course_directory_path')
+
+    def __str__(self):
+        return self.topic_name
+
+
+class Student(models.Model):
+    tucan_id = models.CharField(max_length=200, primary_key=True)
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.tucan_id
+
+
+
