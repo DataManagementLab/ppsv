8b470a0276e0a006c49b05df1331d02de192067f
christoph.berst <christoph.berst@stud.tu-darmstadt.de>
2021-12-02 13:02:29 +0100

    Datenbankimplementierung in models models zu Adminseite hinzugefügt

--- ppsv/course/admin.py
+++ ppsv/course/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
 from .models import Course
+from .models import Topic
+from .models import Student
+from .models import Group
+from .models import TopicSelection
+from .models import TextSaves
 
 admin.site.register(Course)
+admin.site.register(Topic)
+admin.site.register(Student)
+admin.site.register(Group)
+admin.site.register(TopicSelection)
+admin.site.register(TextSaves)
--- ppsv/course/models.py
+++ ppsv/course/models.py
@@ -1,22 +1,51 @@
+import datetime
+
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils import timezone
 
 
 class Course(models.Model):
-    course_name = models.CharField(max_length=200)
-    course_type = models.CharField(max_length=200)
-    register_from = models.DateTimeField('date registration opens')
+    name = models.CharField(max_length=200)
+
+    # maybe we need more choices
+    SEMINAR = 'SE'
+    PRAKTIKUM = 'PR'
+    COURSE_TYPE_CHOICES = [
+        (SEMINAR, 'Seminar'),
+        (PRAKTIKUM, 'Praktikum'),
+    ]
+    type = models.CharField(max_length=2, choices=COURSE_TYPE_CHOICES, default=SEMINAR)
+
+    registration_start = models.DateTimeField('date registration starts', default=timezone.now)
     registration_deadline = models.DateTimeField('registration deadline')
-    course_description = models.TextField('course description')
-    max_participants = models.IntegerField('maximum number of participants', default=9999)
-    cp = models.IntegerField('CP')
-    # maybe use choices for category and field?
-    category = models.CharField(max_length=200)
-    faculty = models.CharField(max_length=200)
+    description = models.TextField('course description')
+    max_participants = models.IntegerField('maximum number of participants', default=9999,
+                                           validators=[MaxValueValidator(9999), MinValueValidator(0)])
+    cp = models.IntegerField('CP', validators=[MaxValueValidator(100), MinValueValidator(0)])
+    # maybe we need more choices
+    OBLIGATORY = 'PF'
+    OPTIONAL = 'WA'
+    COURSE_CATEGORY_CHOICES = [
+        (OBLIGATORY, 'obligatory'),
+        (OPTIONAL, 'optional'),
+    ]
+    category = models.CharField(max_length=2, choices=COURSE_CATEGORY_CHOICES, default=OPTIONAL)
+
+    # maybe we need more choices
+    INFORMATIK = 'FB20'
+    PHYSIK = 'FB05'
+    COURSE_FACULTY_CHOICES = [
+        (INFORMATIK, 'FB20 Informatik'),
+        (PHYSIK, 'FB05 PHYSIK'),
+    ]
+    faculty = models.CharField(max_length=4, choices=COURSE_FACULTY_CHOICES)
+
     # organizer or instructors?
     organizer = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.course_name
+        return self.name
 
 
 def course_directory_path(instance, filename):
@@ -26,17 +55,18 @@ def course_directory_path(instance, filename):
 
 class Topic(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    topic_name = models.CharField(max_length=200)
-    max_participants = models.IntegerField('maximum number of participants', default=999)
-    topic_description = models.TextField('topic description')
-    topic_file = models.FileField(upload_to='course_directory_path')
+    title = models.CharField(max_length=200)
+    max_participants = models.IntegerField('maximum number of participants', default=9999,
+                                           validators=[MaxValueValidator(9999), MinValueValidator(0)])
+    description = models.TextField('topic description')
+    file = models.FileField(upload_to='course_directory_path', blank=True)
 
     def __str__(self):
-        return self.topic_name
+        return self.title
 
 
 class Student(models.Model):
-    tucan_id = models.CharField(max_length=200, primary_key=True)
+    tucan_id = models.CharField(max_length=8, primary_key=True)
     firstname = models.CharField(max_length=200)
     lastname = models.CharField(max_length=200)
     email = models.EmailField()
@@ -45,4 +75,22 @@ class Student(models.Model):
         return self.tucan_id
 
 
+class Group(models.Model):
+    students = models.ManyToManyField(Student)
+    size = models.IntegerField('size of group', default=1, validators=[MaxValueValidator(99), MinValueValidator(1)])
+    assignments = models.ManyToManyField(Topic, blank=True)
+
+
+class TopicSelection(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    motivation = models.TextField('motivation text')
+    priority = models.IntegerField('priority', default=1,
+                                   validators=[MaxValueValidator(99), MinValueValidator(1)])
+
+
+class TextSaves(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    topic = models.CharField(max_length=200)
+    motivation = models.TextField('motivation text')
 
