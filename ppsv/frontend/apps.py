0fc661c40e1a9f2d3a24d4efa27e1bf76af5a230
emre.berber <emre.berber@stud.tu-darmstadt.de>
2022-01-02 19:03:42 +0100

    Fixed mainly typos in comments

--- ppsv/frontend/apps.py
+++ ppsv/frontend/apps.py
@@ -3,12 +3,8 @@ This file configures the application for the frontend.
 """
 from django.apps import AppConfig
 
+
 class FrontendConfig(AppConfig):
-    """ Content types configuration
-    Configures the pluggable application for the frontend
-    :attr ContenttypesConfig.name: Defines which application the configuration applies to
-    :type ContenttypesConfig.name: str
-    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'frontend'
 
--- ppsv/frontend/static/js/login.js
+++ ppsv/frontend/static/js/login.js
@@ -3,7 +3,7 @@ console.clear();
 var loginBtn = document.getElementById('login');
 var signupBtn = document.getElementById('signup');
 
-/* When the login button is clicked than the login window opens and the sing up window closes.
+/* When the login button is clicked, the login window opens and the sign up window closes.
    If the login window was already open it closes and the sign up window opens instead. */
 loginBtn.addEventListener('click', (e) => {
 	let parent = e.target.parentNode.parentNode;
@@ -17,7 +17,7 @@ loginBtn.addEventListener('click', (e) => {
 	});
 });
 
-/* When the sign up button is clicked than the sign up window opens and the login window closes
+/* When the sign up button is clicked, the sign up window opens and the login window closes
    If the sign up window was already open it closes and the login window opens instead. */
 signupBtn.addEventListener('click', (e) => {
 	let parent = e.target.parentNode;
