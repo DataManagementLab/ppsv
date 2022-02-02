34d96da3f9267417b810e133ef16d345d67950ba
premiumbutler <felixgraf39@yahoo.de>
2022-02-02 18:59:37 +0100

    uploaded files can now be opened by clicking on them (adminsite only) - added MEDIA_URL & static serve

--- ppsv/ppsv/settings.py
+++ ppsv/ppsv/settings.py
@@ -143,7 +143,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
