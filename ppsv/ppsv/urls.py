58ab5f49b6e131bd0b79e4984d107b269b11b680
christoph.berst <christoph.berst@stud.tu-darmstadt.de>
2022-03-17 15:03:33 +0100

    Fixed a Warning that occurred because of an old i18n url scheme

--- ppsv/ppsv/urls.py
+++ ppsv/ppsv/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls')),
-    path(r'^i18n/', include('django.conf.urls.i18n')),
+    path('i18n/', include('django.conf.urls.i18n')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
