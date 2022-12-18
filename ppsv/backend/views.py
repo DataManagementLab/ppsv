b5729f784237edce36c391dc38806c96fb25d102
JackDaniel003 <daniel.joest@t-online.de>
2022-12-18 23:11:18 +0100

    updated header layout, added block to display additional content; implemented export for the manual assignment; added new page for the file download (added page in pages folder, added new view in views.py and added new url in urls.py); added class TopicSelectionResource in admin.py to format the data in preparation of the download

--- ppsv/backend/views.py
+++ ppsv/backend/views.py
@@ -1,8 +1,18 @@
+import csv
+import zipfile
+
+from django.http import FileResponse, HttpResponse
+
+from course.models import Topic
+from backend.models import Assignment
+from .admin import TopicSelectionResource
 from .pages.assignment_page import assignment_page
 from .pages.home_page import home_page
+from .pages.export_applications_and_assignments_page import export_applications_and_assignments_page
 
 
 # Create your views here.
+
 
 def assignment_view(request):
     assignment_page(request)
@@ -10,3 +20,7 @@ def assignment_view(request):
 
 def home_view(request):
     home_page(request)
+
+
+def export_applications_and_assignments_view(request):
+    export_applications_and_assignments_page(request)
