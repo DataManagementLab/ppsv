e24a31db763fc5d0e8865e1cfecc177889f69255
JackDaniel003 <daniel.joest@t-online.de>
2022-12-18 23:12:28 +0100

    updated header layout, added block to display additional content; implemented export for the manual assignment; added new page for the filedownload (added page in pages folder, added new view in views.py and added new url in urls.py); added class TopicSelectionResource in admin.py to format the data in preparation of the download

--- ppsv/backend/pages/home_page.py
+++ ppsv/backend/pages/home_page.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
 def home_page(request):
+    """The view for the home page.
+
+    :param request: the given request send by the home html-page
+
+    :return: a render() object
+    :rtype: HttpResponse
+    """
+    if not request.user.is_staff:
+        return redirect(reverse('admin:login') + '?next=' + reverse('backend:home_page'))
+
     return render(request, 'backend/home.html')
