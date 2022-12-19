cb99476d08e181f786b9fa16c8c639c3581ba445
LukasKlenner <Klenner-lukas@t-online.de>
2022-12-19 19:31:30 +0100

    re-add old test for assignment view

--- ppsv/backend/tests.py
+++ ppsv/backend/tests.py
@@ -1,12 +1,14 @@
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
-from django.contrib.auth.models import User
+
 from course.models import Topic, Group, CourseType, Course, Student, TopicSelection
 from .models import Assignment
 
 
+# noinspection PyUnresolvedReferences,DuplicatedCode
 class TestAssignmentModel(TestCase):
 
     @classmethod
@@ -171,6 +173,7 @@ class TestAssignmentModel(TestCase):
 
         self.assertEqual(self.assignment2.assigned_student_to_slot_count, 5)
 
+    # noinspection PyUnusedLocal
     def test_error_if_slot_id_not_unique(self):
         """
         tests if clean() raises a ValidationError if the slot id is not unique
@@ -199,3 +202,351 @@ class TestAssignmentModel(TestCase):
         self.assignment4.accepted_applications.add(self.app_g5_ct2_t2_c2_p1)
         with self.assertRaises(ValidationError):
             self.assignment4.clean()
+
+
+# noinspection DuplicatedCode,PyUnresolvedReferences
+class AssignmentViewTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        """
+        Sets up test data
+        """
+        cls.user1 = User.objects.create_user(username='testuser1', password='12345')
+        cls.user2 = User.objects.create_user(username='testuser2', password='12345')
+        cls.user3 = User.objects.create_user(username='testuser3', password='12345')
+        cls.user4 = User.objects.create_user(username='testuser4', password='12345')
+        cls.user5 = User.objects.create_user(username='testuser5', password='12345')
+        cls.user6 = User.objects.create_user(username='testuser6', password='12345')
+
+        cls.superUser1 = User.objects.create_superuser(username='testsuperuser1', password='12345')
+
+        cls.seminar_type = CourseType.objects.create(type='Seminar')
+
+        cls.course1 = Course.objects.create(registration_deadline=timezone.now(), cp=5, motivation_text=True,
+                                            type=cls.seminar_type, title="course1", created_by=cls.superUser1)
+        cls.course2 = Course.objects.create(registration_deadline=timezone.now(), cp=5, motivation_text=True,
+                                            type=cls.seminar_type, title="course2", created_by=cls.superUser1)
+        cls.topic1A = Topic.objects.create(course=cls.course1, title='topic1A', max_slots=3, min_slot_size=3,
+                                           max_slot_size=5)
+        cls.topic1B = Topic.objects.create(course=cls.course1, title='topic1B', max_slots=3, min_slot_size=3,
+                                           max_slot_size=5)
+        cls.topic2A = Topic.objects.create(course=cls.course2, title='topic2A', max_slots=3, min_slot_size=3,
+                                           max_slot_size=5)
+        cls.topic2B = Topic.objects.create(course=cls.course2, title='topic2B', max_slots=3, min_slot_size=1,
+                                           max_slot_size=1)
+
+        cls.student1 = Student.objects.create(user=cls.user1, tucan_id='ab12eeee')
+        cls.student2 = Student.objects.create(user=cls.user2, tucan_id='bc22eeee')
+        cls.student3 = Student.objects.create(user=cls.user3, tucan_id='cd33eeee')
+        cls.student4 = Student.objects.create(user=cls.user4, tucan_id='de44eeee')
+        cls.student5 = Student.objects.create(user=cls.user5, tucan_id='ef55eeee')
+        cls.student6 = Student.objects.create(user=cls.user6, tucan_id='fg66eeee')
+
+        cls.group1_1_2 = Group.objects.create()
+        cls.group1_1_2.students.add(cls.student1)
+        cls.group1_1_2.students.add(cls.student2)
+
+        cls.group2_3 = Group.objects.create()
+        cls.group2_3.students.add(cls.student3)
+
+        cls.group3_4 = Group.objects.create()
+        cls.group3_4.students.add(cls.student4)
+
+        cls.group4_5 = Group.objects.create()
+        cls.group4_5.students.add(cls.student5)
+
+        cls.group5_6 = Group.objects.create()
+        cls.group5_6.students.add(cls.student6)
+
+        cls.topic_selection1 = TopicSelection.objects.create(group=cls.group1_1_2, topic=cls.topic1A, priority=1)
+        cls.topic_selection1_2 = TopicSelection.objects.create(group=cls.group1_1_2, topic=cls.topic2A, priority=1)
+        cls.topic_selection2 = TopicSelection.objects.create(group=cls.group2_3, topic=cls.topic1A, priority=2)
+        cls.topic_selection3 = TopicSelection.objects.create(group=cls.group3_4, topic=cls.topic1A, priority=3)
+        cls.topic_selection4 = TopicSelection.objects.create(group=cls.group4_5, topic=cls.topic2A, priority=4)
+        cls.topic_selection5 = TopicSelection.objects.create(group=cls.group5_6, topic=cls.topic2A, priority=4)
+        cls.topic_selection1_3 = TopicSelection.objects.create(group=cls.group1_1_2, topic=cls.topic2B, priority=1)
+
+        cls.assignment1 = Assignment.objects.create(topic=cls.topic1A, slot_id=2)
+        cls.assignment1.accepted_applications.add(cls.topic_selection1)
+        cls.assignment1.accepted_applications.add(cls.topic_selection2)
+
+    def test_view_url_exists_at_correct_location(self):
+        """
+        Tests if the URL of the assignment view exists at the correct location.
+        """
+        self.client.force_login(self.superUser1)
+        response = self.client.get(reverse('backend:assignment_page'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_redirect_for_anonymous_user(self):
+        """
+        Tests if an anonymous user is redirected to the admin login page when trying to access the assignment page.
+        """
+        response = self.client.get(reverse('backend:assignment_page'), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response, reverse('admin:login') + '?next=' + reverse('backend:assignment_page'))
+
+    def test_redirect_for_default_user(self):
+        """
+        Tests if an anonymous user is redirected to the admin login page when trying to access the assignment page.
+        """
+        self.client.force_login(self.user1)
+        response = self.client.get(reverse('backend:assignment_page'), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response, reverse('admin:login') + '?next=' + reverse('backend:assignment_page'))
+
+    def test_view_template(self):
+        """
+        Tests if the assignment page view uses the correct template assignment.html.
+        """
+        self.client.force_login(self.superUser1)
+        response = self.client.get(reverse('backend:assignment_page'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'backend/assignment.html')
+
+    def test_assignment_interface(self):
+        """
+        Tests if the assignment view displays the correct interface to make assignments.
+        """
+        self.client.force_login(self.superUser1)
+        response = self.client.get(reverse('backend:assignment_page'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Topics')
+        self.assertContains(response, 'Search course/topic')
+        self.assertContains(response, 'Assignments')
+        self.assertContains(response, 'Applications')
+
+    def test_select_topic(self):
+        """
+        Tests if the response of a select topic action is correct
+        """
+        data = {'action': 'selectTopic',
+                'topics_of_courses': [
+                    {'course': self.course1,
+                     'topics': [
+                         self.topic1A,
+                         self.topic2A
+                     ],
+                     },
+                    {'course': self.course2,
+                     'topics': [
+                         self.topic1B,
+                     ],
+                     }
+                ],
+                'topicID': 1}
+        self.client.force_login(self.superUser1)
+        response = self.client.post(reverse('backend:assignment_page'), data=data)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            {
+                "topicName": "topic1A",
+                "topicMinSlotSize": 3,
+                "topicMaxSlotSize": 5,
+                "topicSlots": 3,
+                "topicCourseName": "course1",
+                "applications": [
+                    {
+                        "students": [
+                            "ab12eeee",
+                            "bc22eeee"],
+                        "applicationID": 1,
+                        "possibleAssignmentsForCollection": 2,
+                        "preference": 1,
+                        "slotID": 2
+                    },
+                    {
+                        "students": [
+                            "cd33eeee"
+                        ],
+                        "applicationID": 3,
+                        "possibleAssignmentsForCollection": 0,
+                        "preference": 2,
+                        "slotID": 2},
+                    {
+                        "students": [
+                            "de44eeee"
+                        ],
+                        "applicationID": 4,
+                        "possibleAssignmentsForCollection": 1,
+                        "preference": 3,
+                        "slotID": -1
+                    }
+                ]
+            }
+        )
+
+    def test_new_Assignment(self):
+        """
+        Tests if the response of a new assignment action is correct
+        """
+
+        if Assignment.objects.filter(topic=self.topic2A, slot_id=1).exists():
+            Assignment.objects.get(topic=self.topic2A, slot_id=1).delete()
+
+        data = {'action': 'newAssignment',
+                'topic_id': 3,
+                'slotID': 1,
+                'applicationID': 5
+                }
+        self.client.force_login(self.superUser1)
+        response = self.client.post(reverse('backend:assignment_page'), data=data)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            {
+                'requestStatus': True,
+                'text': "Saved to Database"
+            }
+        )
+
+        self.assertTrue(
+            Assignment.objects.get(topic=self.topic2A, slot_id=1).accepted_applications.contains(self.topic_selection4))
+
+    def test_new_Assignment_does_not_exists(self):
+        """
+        Tests if the response of a new assignment action is correct when the new Assignment already exists
+        """
+
+        data = {'action': 'newAssignment',
+                'topic_id': 3,
+                'slotID': 1,
+                'applicationID': 69
+                }
+        self.client.force_login(self.superUser1)
+        response = self.client.post(reverse('backend:assignment_page'), data=data)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            {
+                'requestStatus': False,
+                'text': "This assignment exists already"
+            }
+        )
+
+    def test_new_Assignment_not_enough_space_in_slot(self):
+        """
+        Tests if the response of a new assignment action is correct when the new Assignment already exists
+        """
+
+        data = {'action': 'newAssignment',
+                'topic_id': self.topic2B.id,
+                'slotID': 1,
+                'applicationID': self.topic_selection1_3.id
+                }
+        self.client.force_login(self.superUser1)
+        response = self.client.post(reverse('backend:assignment_page'), data=data)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            {
+                'requestStatus': False,
+                'text': "No Space in Slot"
+            }
+        )
+
+    def test_remove_Assignment(self):
+        """
+        Tests if the response of a remove assignment topic action is correct
+        """
+
+        if not Assignment.objects.filter(topic=self.topic2A, slot_id=1).exists():
+            Assignment.objects.create(topic=self.topic2A, slot_id=1)
+
+        Assignment.objects.get(topic=self.topic2A, slot_id=1).accepted_applications.add(self.topic_selection4)
+
+        data = {'action': 'removeAssignment',
+                'slotID': 1,
+                'applicationID': 5
+                }
+        self.client.force_login(self.superUser1)
+        response = self.client.post(reverse('backend:assignment_page'), data=data)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            {
+                'requestStatus': True,
+                'text': "Assignment deleted"
+            }
+        )
+
+        self.assertFalse(Assignment.objects.filter(topic=self.topic2A, slot_id=1).exists())
+
+    def test_remove_Assignment_multiple_groups(self):
+        """
+        Tests if the response of a remove assignment topic action is correct when multiple groups are assigned to a
+        topic
+        """
+
+        if not Assignment.objects.filter(topic=self.topic2A, slot_id=1).exists():
+            Assignment.objects.create(topic=self.topic2A, slot_id=1)
+
+        Assignment.objects.get(topic=self.topic2A, slot_id=1).accepted_applications.add(self.topic_selection4)
+        Assignment.objects.get(topic=self.topic2A, slot_id=1).accepted_applications.add(self.topic_selection5)
+
+        data = {'action': 'removeAssignment',
+                'slotID': 1,
+                'applicationID': 5
+                }
+        self.client.force_login(self.superUser1)
+        response = self.client.post(reverse('backend:assignment_page'), data=data)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            {
+                'requestStatus': True,
+                'text': "Assignment deleted"
+            }
+        )
+
+        self.assertFalse(
+            Assignment.objects.get(topic=self.topic2A, slot_id=1).accepted_applications.contains(self.topic_selection4))
+        self.assertTrue(
+            Assignment.objects.get(topic=self.topic2A, slot_id=1).accepted_applications.contains(self.topic_selection5))
+
+    def test_change_Assignment(self):
+        """
+        Tests if the response of a remove assignment topic action is correct when multiple groups are assigned to a
+        topic
+        """
+
+        if not Assignment.objects.filter(topic=self.topic2A, slot_id=1).exists():
+            Assignment.objects.create(topic=self.topic2A, slot_id=1)
+
+        Assignment.objects.get(topic=self.topic2A, slot_id=1).accepted_applications.add(self.topic_selection4)
+        Assignment.objects.get(topic=self.topic2A, slot_id=1).accepted_applications.add(self.topic_selection5)
+
+        data = {'action': 'changeAssignment',
+                'oldSlotID': 1,
+                'newSlotID': 3,
+                'applicationID': 5
+                }
+        self.client.force_login(self.superUser1)
+        response = self.client.post(reverse('backend:assignment_page'), data=data)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            {
+                'requestStatus': True,
+                'text': "Saved to Database"
+            }
+        )
+
+        self.assertFalse(
+            Assignment.objects.get(topic=self.topic2A, slot_id=1).accepted_applications.contains(self.topic_selection4))
+        self.assertTrue(
+            Assignment.objects.get(topic=self.topic2A, slot_id=1).accepted_applications.contains(self.topic_selection5))
+
+        self.assertTrue(
+            Assignment.objects.get(topic=self.topic2A, slot_id=3).accepted_applications.contains(self.topic_selection4))
+        self.assertFalse(
+            Assignment.objects.get(topic=self.topic2A, slot_id=3).accepted_applications.contains(self.topic_selection5))
