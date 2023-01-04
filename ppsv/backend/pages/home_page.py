6fde23ac77cda3bae4dd9436f17b7e2a502b2862
JackDaniel003 <daniel.joest@t-online.de>
2023-01-04 23:08:40 +0100

    extended the export function to support an export filtered by faculty

--- ppsv/backend/pages/home_page.py
+++ ppsv/backend/pages/home_page.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
+from course.models import Course
+
 
 def home_page(request):
     """The view for the home page.
@@ -13,4 +15,14 @@ def home_page(request):
     if not request.user.is_staff:
         return redirect(reverse('admin:login') + '?next=' + reverse('backend:home_page'))
 
-    return render(request, 'backend/home.html')
+    args = {}
+
+    faculties = []
+    for course in Course.objects.all():
+        if course.faculty not in faculties:
+            faculties.append(course.faculty)
+    faculties.sort()
+
+    args["faculties"] = faculties
+
+    return render(request, 'backend/home.html', args)
