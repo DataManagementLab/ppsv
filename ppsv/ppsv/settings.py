1f23a314551d11e9901075e9ffdb0733dd954fb5
Benjamin Haettasch <benjamin.haettasch@cs.tu-darmstadt.de>
2023-04-16 14:38:55 +0200

    Introduce new base template
    Common base template for all apps
    Include individual navbar parts from different apps

--- ppsv/ppsv/settings.py
+++ ppsv/ppsv/settings.py
@@ -78,7 +78,9 @@ ROOT_URLCONF = 'ppsv.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
