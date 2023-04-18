4c1b61c7a5a044d263505da4f5dbe8bf8fdcb407
Benjamin Haettasch <benjamin.haettasch@cs.tu-darmstadt.de>
2023-04-19 00:19:48 +0200

    Improve registration form
    Use chosen, add better texts
    Add chosen as dependency

--- ppsv/student/forms.py
+++ ppsv/student/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django.utils.translation import gettext_lazy as _
 
 from base.models import Group, Student
 
@@ -13,3 +14,12 @@ class RegistrationForm(ModelForm):
     class Meta:
         model = Group
         fields = ['students']
+
+    required_css_class = 'required'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.initial = {**self.initial, **kwargs['initial']}
+        self.fields["students"].widget.attrs = {'class': 'chosen-select'}
+        self.fields["students"].label = _("Group members")
+        self.fields["students"].help_text = _("Specify other group members here (if applicable)")
